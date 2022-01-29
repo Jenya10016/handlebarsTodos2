71fd83c0e1610caccdaaa631bfe99385e5019bf0
JukeFinite <emmanueljucaban@gmail.com>
2022-01-29 12:12:44 -0600

    create routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const User = require('./models/User');
+const routes = require('./routes');
 const sequelize = require('./config');
 
 const hbs = exphbs.create({});
@@ -16,7 +16,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-})
+});
