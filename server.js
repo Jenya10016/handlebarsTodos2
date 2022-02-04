6ca6dd44a8c0cc4a0539014b673cf09160e942e5
JukeFinite <emmanueljucaban@gmail.com>
2022-02-03 18:46:04 -0600

    fix auth

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionSettings));
 app.use(routes);
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 });
